6d05da380bec44e48b368b52efc6cd9a3fdb8d71
Todd T. Fries <todd@fries.net>
2013-04-25 09:47:20 -0500

    cleanse and make it just copy packets between two tun devices

--- tunbridge.c
+++ tunbridge.c
@@ -1,4 +1,3 @@
-/*	$Id$ */
 /*
  * Copyright (c) 2005 Kenjiro Cho
  * All rights reserved.
@@ -77,7 +76,7 @@ struct tb_stats {
 
 void usage(void);
 void sig_handler(int);
-int event_loop(int);
+int event_loop(int, int);
 void print_stats(void);
 void enqueue(struct queue *, struct pbuf *);
 struct pbuf *dequeue(struct queue *);
@@ -86,18 +85,15 @@ void pbuf_dump(struct pbuf *);
 struct	pbuf *read_packet(int);
 int write_packet(int, struct pbuf *);
 int is_pkt_ready(struct queue *, struct timeval *);
-int emulate_loss(int);
 
-const	char *device = "/dev/tun0";
-int	delay_ms = 200;
-long	loss_prob = 0;
+const	char *devicea = "/dev/tun0";
+const	char *deviceb = "/dev/tun1";
 long	bit_err = 0;
 double	shaping = 0;
 int	queue_limit = 500;
 int	report_interval = 5000;		/* report every 5 seconds */
 
 struct	queue *delay_queue;
-struct	timeval delay_time, shape_time;
 struct	tb_stats stats, last_stats;
 int	verbose;
 int	done;
@@ -114,9 +110,7 @@ int	seq;
 
 void usage(void)
 {
-	printf("usage: tunbridge [-qv]\n"
-	       "       [-b bit_error][-d delay_ms][-l queue_limit]\n"
-	       "       [-n device][-p loss_prob][-s shaping[Kb|Mb|Gb]]\n");
+	printf("usage: tunbridge [-qv]\n\n");
 }
 
 void
@@ -133,50 +127,19 @@ sig_handler(int sig)
 int
 main(int argc, char **argv)
 {
-	int	ch, fd;
-	char	*cp;
-	double	val;
+	int	ch, fda, fdb;
 
 	while ((ch = getopt(argc, argv,
 	    "b:d:l:n:p:qs:v")) != EOF) {
 		switch (ch) {
-		case 'b':
-			/* sanity check */
-			val = strtod(optarg, NULL);
-			if (val * 1500 * 8 >= 0x7fffffff)
-				fprintf(stderr,
-				    "bit errror rate %f too high\n", val);
-			else {
-				bit_err = (long)(strtod(optarg, NULL)
-				    * 0x7fffffff);
-				loss_prob = bit_err;
-			}
-			break;
-		case 'd':
-			delay_ms = (int)strtol(optarg, NULL, 0);
-			break;
 		case 'l':
 			queue_limit = (int)strtol(optarg, NULL, 0);
 			break;
 		case 'n':
-			device = optarg;
+			devicea = optarg;
 			break;
-		case 'p':
-			loss_prob = (long)(strtod(optarg, NULL) * 0x7fffffff);
-			break;
-		case 's':
-			shaping = strtod(optarg, &cp);
-			if (cp != NULL) {
-				if (!strcmp(cp, "b"))
-					; /* nothing */
-				else if (!strcmp(cp, "Kb"))
-					shaping *= 1000;
-				else if (!strcmp(cp, "Mb"))
-					shaping *= 1000 * 1000;
-				else if (!strcmp(cp, "Gb"))
-					shaping *= 1000 * 1000 * 1000;
-			}
-			shaping /= 8;	/* convert to bytes per sec */
+		case 'o':
+			deviceb = optarg;
 			break;
 		case 'q':
 			verbose = -1;
@@ -194,153 +157,71 @@ main(int argc, char **argv)
 	signal(SIGHUP, sig_handler);
 	signal(SIGUSR1, sig_handler);
 
-	gettimeofday(&delay_time, NULL);
-	srandom((u_int)delay_time.tv_usec);
-
-	delay_time.tv_sec  = delay_ms / 1000;
-	delay_time.tv_usec = (delay_ms % 1000) * 1000;
-
-	if ((fd = open(device, O_RDWR)) == -1)
-		err(1, "can't open %s", device);
-
-	if ((delay_queue = malloc(sizeof(*delay_queue))) == NULL)
-		err(1, "malloc");
-	delay_queue->tail = NULL;
-	delay_queue->qlen = 0;
-	delay_queue->qlim = queue_limit;
+	if ((fda = open(devicea, O_RDWR)) == -1)
+		err(1, "can't open %s", devicea);
+	if ((fdb = open(deviceb, O_RDWR)) == -1)
+		err(1, "can't open %s", deviceb);
 
 	if (verbose >= 0) {
-		printf("tunbridge started for %s\n", device);
-		printf("  delay:%d ms  queue limit: %d pkts\n",
-		    delay_ms, queue_limit);
-		if (loss_prob != 0) {
-			if (bit_err)
-				printf("  bit error: %.3f\n",
-				    (double)bit_err / 0x7fffffff);
-			else
-				printf("  packet loss: %.3f\n",
-				    (double)loss_prob / 0x7fffffff);
-		}
-		if (shaping != 0)
-			printf("  shaping: %.2f kbps\n", shaping * 8 / 1000);
+		printf("tunbridge started for %s <-> %s\n", devicea, deviceb);
 	}
 
-	event_loop(fd);
+	event_loop(fda, fdb);
 
 	flushqueue(delay_queue);
 	free(delay_queue);
 
-	close(fd);
+	close(fda);
+	close(fdb);
 
 	return 0;
 }
 
 int
-event_loop(int fd)
+event_loop(int fda, int fdb)
 {
-	struct	pollfd pfd[1];
+	struct	pollfd pfd[2];
 	struct	timeval cur_time;
 	struct	pbuf *p;
-	long	diff;
 	int	nfds, timeout;
 
 	while (!done) {
-		/* compute the next timeout for poll(2) */
-		if (qempty(delay_queue)) {
-			if (report_interval > 0)
-				timeout = report_interval;
-			else
-				timeout = -1;
-		} else {
-			gettimeofday(&cur_time, NULL);
-			p = qhead(delay_queue);
-			timeout = TV_DIFF_INMS(&p->pb_time, &cur_time);
-			if (timeout < 0)
-				timeout = 0;
-		}
+		timeout = report_interval;
 
-		pfd[0].fd = fd;
+		pfd[0].fd = fda;
 		pfd[0].events = POLLIN;
-		nfds = poll(pfd, 1, timeout);
+		pfd[1].fd = fdb;
+		pfd[1].events = POLLIN;
+		nfds = poll(pfd, 2, timeout);
 		if (nfds == -1 && errno == EINTR)
 			continue;
 		if (nfds == -1  ||
-		    (pfd[0].revents & (POLLERR|POLLHUP|POLLNVAL)))
+		    (pfd[0].revents & (POLLERR|POLLHUP|POLLNVAL)) ||
+		    (pfd[1].revents & (POLLERR|POLLHUP|POLLNVAL))
+		)
 			errx(1, "poll");
 
 		gettimeofday(&cur_time, NULL);
-
-		/*
-		 * if we have packets to reinject from the delay queue,
-		 * write them to the tun device.
-		 */
-		while (is_pkt_ready(delay_queue, &cur_time)) {
-			p = dequeue(delay_queue);
-			if (p == NULL)
-				errx(1, "dequeue");
-			diff = TV_DIFF_INMS(&cur_time, &p->pb_time);
-			if (verbose > 0)
-				fprintf(stderr,
-				    "packet ready: %ld ms behind\n", diff);
-			stats.discrepancy += diff;
-			write_packet(fd, p);
-		}
 
 		if (verbose > 0)
 			fprintf(stderr, "poll: nfds=%d, timeout=%d\n",
 			    nfds, timeout);
 
 		/* read packets from device */
-		if (nfds > 0 && (p = read_packet(fd)) != NULL) {
+		if (nfds > 0) {
+			if (pfd[0].revents & POLLIN) {
+				if ((p = read_packet(fda)) != NULL) {
+					p->pb_time = cur_time;
 
-			/*
-			 * compute the departing time of the packet
-			 */
-			/* first, check if shaping is in effect */
-			if (shaping == 0 || TV_LE(&shape_time, &cur_time))
-				shape_time = cur_time;
-			else
-				stats.shaped++;
-
-			p->pb_time = shape_time;
-
-			/* then, add the delay */
-			p->pb_time.tv_sec += delay_time.tv_sec;
-			p->pb_time.tv_usec += delay_time.tv_usec;
-			if (p->pb_time.tv_usec > 1000000) {
-				p->pb_time.tv_usec -= 1000000;
-				p->pb_time.tv_sec++;
-			}
-
-			/* if shaping is set, update the shape time. */
-			if (shaping != 0) {
-				double delta;
-
-				/*
-				 * compute the time required to send this
-				 * packet in seconds, and add it to the
-				 * supposed finish time.  we need to subtract
-				 * 4 bytes for the address family prepended
-				 * for the tun device.
-				 */
-				delta = (double)(p->pb_len - 4) / shaping;
-				shape_time.tv_sec += (long)delta;
-				delta -= (long)delta;
-				shape_time.tv_usec += delta * 1000000;
-				if (shape_time.tv_usec > 1000000) {
-					shape_time.tv_usec -= 1000000;
-					shape_time.tv_sec++;
+					write_packet(fdb, p);
 				}
 			}
+			if (pfd[1].revents & POLLIN) {
+				if ((p = read_packet(fdb)) != NULL) {
+					p->pb_time = cur_time;
 
-			/* emulate packet loss */
-			if ((loss_prob != 0 && emulate_loss(p->pb_len) == 1) ||
-			    delay_queue->qlen >= delay_queue->qlim) {
-				free(p);
-				stats.dropped++;
-			} else {
-				/* enqueue the packet to the delay queue */
-				enqueue(delay_queue, p);
+					write_packet(fda, p);
+				}
 			}
 		}
 
@@ -492,16 +373,3 @@ is_pkt_ready(struct queue *q, struct timeval *cur)
 	return (0);
 }
 
-int
-emulate_loss(int len)
-{
-	if (bit_err > 0) {
-		if (random() < bit_err * len * 8)
-			return (1);
-		return (0);
-	}
-
-	if (random() < loss_prob)
-		return (1);
-	return (0);
-}
